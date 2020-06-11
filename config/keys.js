05e74cfbb1b058797977b28a57bdde251cbc9cd3
ak-353 <ak-353@mail.ru>
2020-06-12 02:25:17 +0300

    add new ver.

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI:'mongodb+srv://inga-123:VmWft4OAXsuMgKfq@cluster0-rc5mm.mongodb.net/test?retryWrites=true&w=majority'
+}
---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const items = require('./routes/api/items');
+
+const app =express();
+
+//BodyParser Middleware
+app.use(bodyParser.json());
+
+//DB Config
+const db = require('./config/keys').mongoURI;
+
+//Connect to Mongo
+mongoose
+ .connect(db)
+ .then(()=> console.log('MongoDB is connected'))
+ .catch(err => console.log(err));
+
+ //Use Routes
+
+ app.use('/api/items', items);
+
+ const port = process.env.PORT || 5000;
+
+ app.listen(port, () => console.log(`Server startet on port ${port}`));
+
+
