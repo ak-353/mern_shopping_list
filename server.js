0236a60774f77676d9def353c544676acafe7de3
ak-353 <ak-353@mail.ru>
2020-06-17 17:56:52 +0300

    ver 1.4

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ mongoose
      
      app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-     }));
+     });
  }
 
  const port = process.env.PORT || 5000;
